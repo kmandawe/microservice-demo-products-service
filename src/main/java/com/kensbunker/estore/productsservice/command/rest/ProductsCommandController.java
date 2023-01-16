0d1b77f9c2dfc0bca8a794d9fb24726c9e770848
kmandawe <kdmandawe06@gmail.com>
2023-01-16 18:13:54 +0800

    Validating Request Body, Bean Validation

--- src/main/java/com/kensbunker/estore/productsservice/command/rest/ProductsCommandController.java
+++ src/main/java/com/kensbunker/estore/productsservice/command/rest/ProductsCommandController.java
@@ -5,6 +5,7 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -20,7 +21,7 @@ public class ProductsCommandController {
   }
   
   @PostMapping
-  public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+  public String createProduct(@Valid @RequestBody CreateProductRestModel createProductRestModel) {
     CreateProductCommand createProductCommand = CreateProductCommand.builder()
             .price(createProductRestModel.getPrice())
             .quantity(createProductRestModel.getQuantity())
